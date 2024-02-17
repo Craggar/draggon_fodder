ebebc21962ef40ae0b866e4df79c23a5a1ccbefa
Craig Gilchrist <craig.a.gilchrist@gmail.com>
2024-02-17 20:13:18 +0000

    more linting on requires

--- app/require.rb
+++ app/require.rb
@@ -1,6 +1,6 @@
-require "lib/drive/require.rb"
+require "lib/drive/require"
 
-require "app/concerns/uses_state.rb"
-require "app/scenes/base.rb"
-require "app/scenes/game.rb"
-require "app/scenes/menu.rb"
+require "app/concerns/uses_state"
+require "app/scenes/base"
+require "app/scenes/game"
+require "app/scenes/menu"
